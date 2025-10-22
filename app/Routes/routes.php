b59c59ce0efaa5c35eb80664732bf2adec3075a9
viitorags <vitorgabriels1000@gmail.com>
2025-10-22 18:38:10 -0300

    feat(routes): implementando endpoints de API RESTful para recursos do sistema

--- app/Routes/routes.php
+++ app/Routes/routes.php
@@ -1,5 +1,10 @@
 <?php
 
+require_once __DIR__ . "/../Controllers/userController.php";
+require_once __DIR__ . "/../Controllers/productController.php";
+require_once __DIR__ . "/../Controllers/clientController.php";
+require_once __DIR__ . "/../Controllers/ordersController.php";
+
 class Router
 {
     private $routes = [];
@@ -21,6 +26,7 @@ class Router
 
 $router = new Router();
 
+// FrontEnd EndPoints
 $router->add('GET', '/', function () {
     include __DIR__ . "/../Views/index.php";
 });
@@ -38,4 +44,88 @@ $router->add('GET', '/orders', function () {
 });
 $router->add('GET', '/customer', function () {
     include __DIR__ . "/../Views/customer.php";
+});
+
+// APIs EndPoints
+$router->add('GET', '/api/produtos', function () {
+    $controller = new ProductController();
+    $controller->getAllProducts();
+});
+
+$router->add('POST', '/api/produtos', function () {
+    $controller = new ProductController();
+    $controller->createNewProduct();
+});
+
+$router->add('PUT', '/api/produtos/', function () {
+    $controller = new ProductController();
+    $controller->updateProduct();
+});
+
+$router->add('DELETE', '/api/produtos', function () {
+    $controller = new ProductController();
+    $controller->deleteProduct();
+});
+
+// User API endpoints
+$router->add('GET', '/api/users', function () {
+    $controller = new UserController();
+    $controller->getUser();
+});
+
+$router->add('POST', '/api/users', function () {
+    $controller = new UserController();
+    $controller->createUser();
+});
+
+$router->add('PUT', '/api/users', function () {
+    $controller = new UserController();
+    $controller->updateUser();
+});
+
+$router->add('DELETE', '/api/users', function () {
+    $controller = new UserController();
+    $controller->deleteUser();
+});
+
+// Client API endpoints
+$router->add('GET', '/api/clients', function () {
+    $controller = new ClientController();
+    $controller->getClient();
+});
+
+$router->add('POST', '/api/clients', function () {
+    $controller = new ClientController();
+    $controller->createNewClient();
+});
+
+$router->add('PUT', '/api/clients', function () {
+    $controller = new ClientController();
+    $controller->updateClient();
+});
+
+$router->add('DELETE', '/api/clients', function () {
+    $controller = new ClientController();
+    $controller->deleteClient();
+});
+
+// Order API endpoints
+$router->add('GET', '/api/orders', function () {
+    $controller = new OrdersController();
+    $controller->getOrders();
+});
+
+$router->add('POST', '/api/orders', function () {
+    $controller = new OrdersController();
+    $controller->createNewOrder();
+});
+
+$router->add('PUT', '/api/orders', function () {
+    $controller = new OrdersController();
+    $controller->updateOrder();
+});
+
+$router->add('DELETE', '/api/orders', function () {
+    $controller = new OrdersController();
+    $controller->deleteOrder();
 });
