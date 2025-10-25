a4b3c49341ec7f2f19d669b10012858d66f5fe2f
viitorags <vitorgabriels1000@gmail.com>
2025-10-25 20:48:56 -0300

    feat(css): melhorias de responsividade e usabilidade em tabelas e dashboard

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ ini_set('log_errors', 1);
 
 ini_set('session.cookie_httponly', 1);
 ini_set('session.use_only_cookies', 1);
-ini_set('session.cookie_secure', 0);
+ini_set('session.cookie_secure', 1);
 ini_set('session.cookie_samesite', 'Lax');
 
 if (session_status() === PHP_SESSION_NONE) {
