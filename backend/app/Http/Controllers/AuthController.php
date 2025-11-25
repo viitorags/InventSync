ffd3597f8f0319c4aafba30cd435699aaf9ca945
viitorags <vitorgabriels1000@gmail.com>
2025-11-25 16:42:48 -0300

    feat(auth): implementando endpoints de login, registro, logout e consulta de usuário

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -1,27 +1,52 @@
 <?php
 
+namespace App\Http\Controllers;
+
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Http\Resources\UserResource;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    private $user;
-
-    public function __construct(User $user)
+    public function login(LoginRequest $request)
     {
-        //
-    }
-
-    public function login()
-    {
-        try {
-        } catch (\Exception | PDOException $err) {
-            throw $err->getMessage();
+        $data = $request->validated();
+        $user = User::where('user_email', $data['user_email'])->first();
+        if (! $user || ! Hash::check($data['user_password'], $user->user_password)) {
+            return response()->json([
+                'message' => 'Credenciais Inválidas'
+            ], 401);
+        } else {
+            $token = auth()->guard('api')->login($user);
+            return response()->json([
+                'acess_token' => $token,
+                'token_type' => 'bearer',
+            ]);
         }
     }
 
-    public function register()
+    public function register(RegisterRequest $request)
     {
-        //
+        $user = User::create($request->validate());
+        $token = auth()->guard('api')->login($user);
+        return response()->json([
+            'user' => new UserResource($user),
+            'acess_token' => $token,
+            'token_type' => 'bearer',
+        ]);
+    }
+
+    public function logout()
+    {
+        auth()->guard('api')->logout();
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    public function me()
+    {
+        return response()->json(auth()->guard('api')->user());
     }
 }
